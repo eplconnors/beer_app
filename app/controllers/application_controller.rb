c9330595929ab64fd5bee0235d12b03a9432d037
Tyler Seawell <tylerseawell4@gmail.com>
2016-05-18 22:35:32 +0000

    white background for feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,13 +28,13 @@ def authenticate_any!
     end
 end
 
-def after_sign_in_path_for(resource_or_scope)
-    if resource.sign_in_count == 1
-       new_product_path
-    else
-       root_path
-    end
-end
+# def after_sign_in_path_for(resource_or_scope)
+#     if resource.sign_in_count == 1
+#       new_product_path
+#     else
+#       root_path
+#     end
+# end
 
 def after_sign_in_path_for(resource)
  if current_business_user
